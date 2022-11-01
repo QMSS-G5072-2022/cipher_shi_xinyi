b07d41f1b6e9528d7b1fdfbdab2b0c9b2ec04ac6
Xinyi Shi <73629497+shixyqd@users.noreply.github.com>
2022-11-01 00:44:14 -0400

    cipher with docstings

--- src/cipher_xs2474/cipher_xs2474.py
+++ src/cipher_xs2474/cipher_xs2474.py
@@ -1,4 +1,29 @@
 def cipher(text, shift, encrypt=True):
+    
+    """
+    An algorithm for performing encryption or decryption which takes in original text and number of shift and returns the transformed text. 
+    
+    Parameters
+    ----------
+    text: your original text (string)
+    shift: the number of shift in the alphabet that you would like to have for your text (integer)
+    encrypt: transforming the text in encryption or decryption (boolean)
+    
+    Returns
+    ----------
+    The new transformed text.
+    
+    Examples
+    ---------
+    from cipher_xs2474 import cipher_xs2474
+    cipher_xs2474.cipher('word, 2, encrypt = True)
+    'yqtf'
+    
+    from cipher_xs2474 import cipher_xs2474
+    cipher_xs2474.cipher('yqtf', 2, encrypt = False)
+    'word'
+    """
+    
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
